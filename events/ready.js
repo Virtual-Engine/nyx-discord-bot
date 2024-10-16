4e12c3283ac2ddb204b2c1a738c50899d011bb1f
Zenith <137690070+zenith-engine@users.noreply.github.com>
2024-10-16 20:44:53 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -8,20 +8,5 @@ module.exports = {
     once: true,
     execute(client) {
         log("info", `Login : ${client.user.username}`);
-        log("done", `Embed Starting Send At ${process.env.salon_starting}`);
-
-        const Embed = new EmbedBuilder()
-            .setTitle("**Starting L0yy**")
-            .setDescription(`L0yy c'est connecte a ${moment().format("HH:mm:ss YYYY-MM-DD")}`)
-            .setColor("#7f00ff")
-            .setTimestamp()
-
-        client.channels.cache.get(process.env.salon_starting).send({ embeds: [Embed] })
-
-        client.user.setPresence({
-            activities: [{ name: 'The best bot', type: ActivityType.Streaming, url: "https://www.twitch.tv/punisherenlive" }],
-
-        });
-
     },
 };
